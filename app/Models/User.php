61c7fadb43a88c981280f56f0e0632532b937505
bukanteguhbiasa <teguhwin8@gmail.com>
2021-11-26 01:35:02 +0700

    login api

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,13 +11,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasAdvancedFilter;
-    use SoftDeletes;
-    use Notifiable;
-    use HasFactory;
+    use HasAdvancedFilter, SoftDeletes, Notifiable, HasFactory, HasApiTokens;
 
     public $table = 'users';
 
